5f3904f4f0d9b023f95def55df084c04361f35dd
CC-hkobashi <hiroshi.kobashikawa@casleyconsulting.co.jp>
2021-10-15 18:16:03 +0900

    12th commit
    # What
    - docker-composeコマンドを無効化

--- scripts/Application.sh
+++ scripts/Application.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 source ~/.profile
-cd Rails-myapp
-docker-compose stop
-docker-compose up
+#cd Rails-myapp
+#docker-compose stop
+#docker-compose up
 # docker exec rails-myapp_web_1 rails db:create
 # docker exec rails-myapp_web_1 rails db:migrate
 docker exec rails-myapp_web_1 bundle install

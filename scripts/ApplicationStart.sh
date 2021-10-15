5fe92838d1f2c219576e2a493fbb219e8428de5a
CC-hkobashi <hiroshi.kobashikawa@casleyconsulting.co.jp>
2021-10-15 19:13:18 +0900

    15th
    #What
    - ApplicationStart.shを削除

--- scripts/ApplicationStart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-source ~/.profile
-# docker-compose up
-# docker exec rails-myapp_web_1 rails db:create
-# docker exec rails-myapp_web_1 rails db:migrate
-docker exec rails-myapp_web_1 bundle install
